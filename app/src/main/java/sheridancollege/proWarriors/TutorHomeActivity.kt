ca6059e8e423f27827d8b213209df23cd76b2e86
shubh228 <pate1995@sheridancollege.ca>
2022-03-29 19:25:41 -0400

    Added tutor home activity and xml files.

---
+++ app/src/main/java/sheridancollege/proWarriors/TutorHomeActivity.kt
@@ -0,0 +1,11 @@
+package sheridancollege.proWarriors
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TutorHomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tutor_home)
+    }
+}
