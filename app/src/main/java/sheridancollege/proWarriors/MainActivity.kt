7274af7b873fb2ef40a6305b80773900e65de9d5
shahvard <shahvard@sheridancollege.ca>
2022-03-29 18:16:07 -0400

    Menu created

--- app/src/main/java/sheridancollege/proWarriors/MainActivity.kt
+++ app/src/main/java/sheridancollege/proWarriors/MainActivity.kt
@@ -2,10 +2,19 @@ package sheridancollege.proWarriors
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflator = menuInflater
+        inflator.inflate(R.menu.main_menu,menu)
+
+        return true
     }
 }
