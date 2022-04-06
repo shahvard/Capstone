7ede535ea87ca9d3e634b3779c4148bbb15a1552
shahvard <shahvard@sheridancollege.ca>
2022-04-06 09:31:30 -0400

    Student Home Page updated.

--- app/src/main/java/sheridancollege/proWarriors/Student/StudentHomePage.kt
+++ app/src/main/java/sheridancollege/proWarriors/Student/StudentHomePage.kt
@@ -9,8 +9,12 @@ import android.widget.TextView
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
 import com.google.firebase.firestore.auth.User
 import com.google.firebase.ktx.Firebase
 import org.w3c.dom.Text
@@ -34,10 +38,37 @@ class StudentHomePage : AppCompatActivity() {
 
 
 
+        val postListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+               if( dataSnapshot.child("Students") != null) {
+                    val data = dataSnapshot.child("Students")
+                    val name =  data.child("shahvard").child("firstName").value.toString()
+                    heading.text = "Welcome $name"
+
+                //Toast.makeText(this@,"Inside",Toast.LENGTH_SHORT).show()
+                }
+                // Get Post object and use the values to update the UI
+                //val post = dataSnapshot.getValue<Post>()
+                // ...
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Getting Post failed, log a message
+                //Log.w(TAG, "loadPost:onCancelled", databaseError.toException())
+            }
+
+        }
+        database.addValueEventListener(postListener)
+        /*if (username != null) {
+            var a = database.child("Students").child(username)
+            a.get().addOnSuccessListener { document->
+               document.getValue<String>()
+            }
+        }*/
 
 
 
-        heading.text = "Welcome " + intent.getStringExtra("studentName")
+       // heading.text = "Welcome " + intent.getStringExtra("studentName")
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
