2e0779afc8f45a4fc1cafeac5d48d512e95bb5fa
shubh228 <pate1995@sheridancollege.ca>
2022-04-06 14:29:08 -0400

    Log out implementation in details page.

--- app/src/main/java/sheridancollege/proWarriors/Profile/StudentDetailsActivity.kt
+++ app/src/main/java/sheridancollege/proWarriors/Profile/StudentDetailsActivity.kt
@@ -1,19 +1,29 @@
 package sheridancollege.proWarriors.Profile
 
+import android.content.DialogInterface
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import sheridancollege.proWarriors.Login.LoginActivity
 import sheridancollege.proWarriors.R
 import sheridancollege.proWarriors.Student.StudentEntity
+import sheridancollege.proWarriors.Student.StudentHomePage
+import sheridancollege.proWarriors.Tutor.TutorHomeActivity
 
 class StudentDetailsActivity : AppCompatActivity() {
+    var student=StudentEntity.student
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_student_details)
 
-         var student=StudentEntity.student
         var first = findViewById<TextView>(R.id.fName)
         var last = findViewById<TextView>(R.id.lName)
         var phone = findViewById<TextView>(R.id.cNumber)
@@ -39,6 +49,43 @@ class StudentDetailsActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         val inflator = menuInflater
         inflator.inflate(R.menu.details_menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean{
+
+        when(item.title.toString()){
+            "Home"->{
+                var intent = Intent(this, StudentHomePage::class.java)
+                startActivity(intent)
+            }
+            "View As Tutor" -> {
+                if(student.isTutor == true){
+                    var intent = Intent(this, TutorHomeActivity::class.java)
+                    startActivity(intent)
+                }
+                else{
+                    val dialogBuilder = AlertDialog.Builder(this)
+                    dialogBuilder.setMessage("You do not have an access to tutor login.")
+                        .setCancelable(false)
+                        .setNegativeButton("Okay", DialogInterface.OnClickListener {
+                                dialog, id -> dialog.cancel()
+                        })
+                    val alert = dialogBuilder.create()
+                    alert.setTitle("Tutor Access denied.")
+                    alert.show()
+                }
+            }
+            "Logout"->{
+                Firebase.auth.signOut()
+                var intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+                Toast.makeText(
+                    this, "Successfully logged out.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
         return true
     }
 }
