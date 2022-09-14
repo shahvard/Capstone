152a1ec2e8061e8eeda9c16c9ef0a11bfd0c3c39
shubh228 <pate1995@sheridancollege.ca>
2022-09-14 09:11:03 -0400

    Working on SSO

--- app/src/main/java/sheridancollege/proWarriors/StudentFragments/StudentLoginFragment.kt
+++ app/src/main/java/sheridancollege/proWarriors/StudentFragments/StudentLoginFragment.kt
@@ -2,6 +2,7 @@ package sheridancollege.proWarriors.StudentFragments
 
 import android.content.ContentValues.TAG
 import android.content.Intent
+import android.net.Credentials
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -12,6 +13,9 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.navigation.findNavController
+import com.auth0.android.Auth0
+import com.auth0.android.authentication.AuthenticationException
+import com.auth0.android.provider.WebAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -22,12 +26,16 @@ import sheridancollege.proWarriors.R
 import sheridancollege.proWarriors.Student.StudentActivity
 import sheridancollege.proWarriors.Student.StudentEntity
 import sheridancollege.proWarriors.Student.stu
+import javax.security.auth.callback.Callback
+import javax.security.auth.callback.CallbackHandler
 
-class StudentLoginFragment : Fragment() {
+class  StudentLoginFragment : Fragment() {
 
     private lateinit var auth: FirebaseAuth
     private lateinit var userName:String
     private lateinit var password:String
+    private lateinit var account: Auth0
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -35,6 +43,12 @@ class StudentLoginFragment : Fragment() {
        // Firebase.auth.signOut()
 
         val view = inflater.inflate(R.layout.fragment_student_login, container, false)
+
+        // Set up the account object with the Auth0 application details
+        account = Auth0(
+            "MZBxOMznLoWJp5GIawlJAMRDUCAchuj8",
+            "dev-gcybgobx.us.auth0.com"
+        )
 
 
         auth = Firebase.auth
@@ -62,6 +76,37 @@ class StudentLoginFragment : Fragment() {
         }
 
         view.findViewById<Button>(R.id.loginButton).setOnClickListener(){
+
+            WebAuthProvider.login(account)
+                .withScheme("demo")
+                .withScope("openid profile email")
+                // Launch the authentication passing the callback where the results will be received
+                .start(this.requireContext(), object : Callback<Credentials, AuthenticationException>,
+                    com.auth0.android.callback.Callback<com.auth0.android.result.Credentials, AuthenticationException> {
+                    // Called when there is an authentication failure
+                    override fun onFailure(exception: AuthenticationException) {
+                        // Something went wrong!
+                    }
+
+                    // Called when authentication completed successfully
+                    fun onSuccess(credentials: Credentials) {
+                        // Get the access token from the credentials object.
+                        // This can be used to call APIs
+                        //val accessToken = credentials.accessToken
+                    }
+
+                    override fun onSuccess(result: com.auth0.android.result.Credentials) {
+                        TODO("Not yet implemented")
+                    }
+                })
+
+
+
+
+
+
+
+        /*
         userName = view.findViewById<TextView>(R.id.userNameText).text.toString()
         password= view.findViewById<TextView>(R.id.passwordText).text.toString()
             auth.signInWithEmailAndPassword(userName, password)
@@ -93,13 +138,13 @@ class StudentLoginFragment : Fragment() {
                                    //Toast.makeText(this.context,"You are not a student",Toast.LENGTH_SHORT).show()
                                }
                            }
-                      /* }
+                      *//* }
                         else{
                            Toast.makeText(
                                this.context, "Please verify your email to Login",
                                Toast.LENGTH_SHORT
                            ).show()
-                       }*/
+                       }*//*
                     } else {
                         Log.w(TAG, "signInWithEmail:failure", task.exception)
                         Toast.makeText(
@@ -107,8 +152,9 @@ class StudentLoginFragment : Fragment() {
                             Toast.LENGTH_SHORT
                         ).show()
                     }
-                }
+                }*/
         }
         return view
     }
+
 }
