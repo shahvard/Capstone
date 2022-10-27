a044a8be6572e1f845721d0d9e347178f48b6770
shubh228 <pate1995@sheridancollege.ca>
2022-10-27 00:40:10 -0400

    Navigation drawer and bottom nav done for both.

--- app/src/main/java/sheridancollege/proWarriors/Tutor/TutorActivity.kt
+++ app/src/main/java/sheridancollege/proWarriors/Tutor/TutorActivity.kt
@@ -1,79 +1,99 @@
 package sheridancollege.proWarriors.Tutor
 
-import android.content.DialogInterface
-import android.content.Intent
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
+import android.util.Log
+import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
-import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.ktx.auth
-import com.google.firebase.database.DatabaseReference
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import sheridancollege.proWarriors.R
-import sheridancollege.proWarriors.Student.StudentActivity
+import java.io.File
 
 class TutorActivity : AppCompatActivity() {
-   /* private lateinit var database: DatabaseReference
-    private lateinit var heading: TextView
-    private var stud: Boolean = false*/
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var storageRef: StorageReference
+    private lateinit var username:String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tutor)
 
+        val user = Firebase.auth.currentUser
+        user?.let {
+            val email = user.email
+            username = email?.split("@")?.get(0).toString()
+        }
+
+        storageRef = FirebaseStorage.getInstance().reference.child("profile_images/$username.jpg")
+
+        val drawerLayout = findViewById<DrawerLayout>(R.id.tutor_drawer_layout)
+        val navView = findViewById<NavigationView>(R.id.tutor_nav_view)
+        val bottomView = findViewById<BottomNavigationView>(R.id.tutor_bottom_navigation)
         val navController = this.findNavController(R.id.tutorNavHost)
-        NavigationUI.setupActionBarWithNavController(this, navController)
+        val img = findViewById<ImageView>(R.id.hamburgerMenuIcon)
+        val head = findViewById<TextView>(R.id.pageTitle)
+        val localFile = File.createTempFile("tempImage", "jpg")
+
+        Log.d("USERNAME", username)
+        GlobalScope.launch {
+            TutorEntity.getTutorDetails(username)
+            var tutor: Tutor = tut.tutor
+            delay(1000)
+            if (tut.tutor != null) {
+                Log.d("TUTOR", tut.tutor.firstName.toString())
+                findViewById<TextView>(R.id.drawerName).text = tut.tutor.firstName.toString()
+                if (tutor.isStudent != null){
+                    findViewById<TextView>(R.id.drawerAccount).text = "Tutor"
+                }else{
+                    findViewById<TextView>(R.id.drawerAccount).text = "Student"
+                }
+            }
+            storageRef.getFile(localFile).addOnSuccessListener {
+                val bitmap = BitmapFactory.decodeFile(localFile.absolutePath)
+                if (bitmap != null){
+                    findViewById<ImageView>(R.id.headerImage).setImageBitmap(bitmap)
+                }else{
+                    findViewById<ImageView>(R.id.headerImage).setImageResource(R.drawable.profile)
+                }
+            }
+        }
+
+        img.setOnClickListener{
+            if (drawerLayout.isDrawerVisible(GravityCompat.START)){
+                drawerLayout.closeDrawer(GravityCompat.END)
+            }else{
+                drawerLayout.openDrawer(GravityCompat.START)
+            }
+        }
+
+        val listener = NavController.OnDestinationChangedListener { controller, destination, arguments ->
+            head.text = destination.label
+        }
+        navController.addOnDestinationChangedListener(listener)
+        appBarConfiguration = AppBarConfiguration(setOf(R.id.tutorHomeFragment, R.id.tutorDetailsFragment), drawerLayout)
+        navView.setupWithNavController(navController)
+        bottomView.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.tutorNavHost)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
-
-    /*override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        val inflator = menuInflater
-        inflator.inflate(R.menu.tutor_menu,menu)
-        return true
-   }*/
-
-    /*override fun onOptionsItemSelected(item: MenuItem): Boolean{
-
-        when(item.title.toString()){
-            "Person"->{
-               *//* var intent = Intent(this, TutorDetailsActivity::class.java)
-                startActivity(intent)*//*
-            }
-            *//*"View As Student" -> {
-                if(stud == true){
-                    var intent = Intent(this, StudentActivity::class.java)
-                    intent.putExtra("tutName", TutorEntity.tutor.firstName)
-                    startActivity(intent)
-                }
-                else{
-                    val dialogBuilder = AlertDialog.Builder(this)
-                    dialogBuilder.setMessage("You are not eligible to access Student Home.")
-                        .setCancelable(false)
-                        .setNegativeButton("Okay", DialogInterface.OnClickListener {
-                                dialog, id -> dialog.cancel()
-                        })
-                    val alert = dialogBuilder.create()
-                    alert.setTitle("Student Access denied.")
-                    alert.show()
-                }
-            }*//*
-            *//*"Logout"->{
-                Firebase.auth.signOut()
-                var intent = Intent(this, LoginActivity::class.java)
-                startActivity(intent)
-                Toast.makeText(
-                    this, "Successfully logged out.",
-                    Toast.LENGTH_SHORT
-                ).show()
-            }*//*
-
-        }
-    return true
-
-
-}
-        */
